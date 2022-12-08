ded36acdbc737b98fd5cad6c31b4fd4f7722b2be
Yufan Zou <yufanzou@Mac-mini.local>
2022-12-08 02:46:04 -0800

    added jrl jar

--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -1,19 +1,20 @@
-package com.example.demo.config;
-
-import javax.sql.DataSource;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class DataSourceConfig {
-	@Bean
-	public DataSource datasource() {
-		return DataSourceBuilder.create()
-			.driverClassName("com.mysql.cj.jdbc.Driver")
-			.url("jdbc:mysql://database-1.clv56pklcwju.us-east-1.rds.amazonaws.com:3306/db")
-			.username("root")
-			.password("password")
-			.build();
-	}
-}
+//package com.example.demo.config;
+//
+//import javax.sql.DataSource;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//
+//@Configuration
+//public class DataSourceConfig {
+//	@Bean
+//	public DataSource datasource() {
+//		return DataSourceBuilder.create()
+//			.driverClassName("com.mysql.cj.jdbc.Driver")
+//			.url("jdbc:mysql://database-1.clv56pklcwju.us-east-1.rds.amazonaws.com:3306/db")
+//			.username("root")
+//			.password("password")
+//			.build();
+//	}
+//}
